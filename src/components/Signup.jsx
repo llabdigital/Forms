bfd949d09181a579303f32a88f9357c0989dd154
your_username <your_email@example.com>
2026-03-31 12:33:07 -0700

    use the action prop. No longer gets an event object gets formData

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,9 @@
 export default function Signup() {
-  function handleSubmit(e) {
+  function handleSubmit(formData) {
     e.preventDefault();
   }
   return (
-    <form onSubmit={handleSubmit}>
+    <form action={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
