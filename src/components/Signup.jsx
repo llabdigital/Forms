83fb3facd90fe881ad2765c5bf7f82e0ed3a09bb
your_username <your_email@example.com>
2026-04-01 13:04:48 -0700

    add validation logic

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -13,7 +13,34 @@ export default function Signup() {
 
     let errors = [];
 
-    if()
+    if (!isEmail(email)) {
+      errors.push('Invalid email address.')
+    }
+
+    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+      errors.push('You must provide a password with at least 6 charecters.')
+    }
+
+    if (!isEqualToOtherValue(password, confirmPassword)) {
+      errors.push('Passwords do not match.');
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push('Please provide both your first and last name.');
+    }
+
+    if (!isNotEmpty(role)) {
+      errors.push('Please select a role');
+    }
+
+    if (!terms) {
+      errors.push('You must agree to the terms and conditions.');
+    }
+
+    if (acquisitionChannel.length === 0) {
+      errors.push('Please select at least one aquisition channel.');
+    }
+
   }
   return (
     <form action={signupAction}>
